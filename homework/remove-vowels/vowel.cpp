54bdda1e3a7c14f9f7c124adb502103eff2d0a05
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-04-20 23:29:11 +0200

    Create body of removeVowels function

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,5 +1,16 @@
 #include "vowel.hpp"
+#include <algorithm>
+#include <vector>
+
+void removeVowels(std::string& string) {
+    std::vector<char> vowels{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'};
+    for (const auto& vowel : vowels) {
+        string.erase(std::remove(string.begin(), string.end(), vowel), string.end());
+    }
+}
 
 void removeVowels(std::vector<std::string>& vector) {
-    std::ignore = vector;
+    for (auto& string : vector) {
+        removeVowels(string);
+    }
 }
