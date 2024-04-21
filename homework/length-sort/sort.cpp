7a0fb25b26d6ec7ec2bf3f21571faa05946ab488
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-04-21 17:41:31 +0200

    Create lengthSort functionality

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,15 @@
+#include "sort.hpp"
+#include <algorithm>
+
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& forward_list) {
+    std::deque<std::string> deque{};
+    for(const auto& element : forward_list)
+    {
+        deque.emplace_back(element);
+    }
+    std::sort(deque.begin(), deque.end());
+    std::sort(deque.begin(), deque.end(), [](std::string lhs, std::string rhs){
+        return lhs.size() < rhs.size();
+    });
+    return deque;
+}
