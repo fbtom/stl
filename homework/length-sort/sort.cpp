2f2b8ea061912d3f7b08fec9402669e59eb33e4e
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-04-21 17:43:20 +0200

    Clang format

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -3,12 +3,11 @@
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& forward_list) {
     std::deque<std::string> deque{};
-    for(const auto& element : forward_list)
-    {
+    for (const auto& element : forward_list) {
         deque.emplace_back(element);
     }
     std::sort(deque.begin(), deque.end());
-    std::sort(deque.begin(), deque.end(), [](std::string lhs, std::string rhs){
+    std::sort(deque.begin(), deque.end(), [](std::string lhs, std::string rhs) {
         return lhs.size() < rhs.size();
     });
     return deque;
